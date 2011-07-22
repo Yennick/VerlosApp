1cc74347acd672452dd539e9ee3a1b15781ffb0d
unknown <alex@.(none)>
2011-07-22 20:45:43 +0200

    begin app: algemene structuur neergezet. index en header/footer toegevoegd

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<p><small>Copyright: lonkeknol 2011</small></p>
+</body>
+</html>
--- includes/header.php
+++ includes/header.php
@@ -2,12 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
+<title>Verloskunde App</title>
 </head>
-
 <body>
-	<?php
-    	echo rand();
-	?>
-</body>
-</html>
+<h1>VerlosApp</h1>
